1c7d6f47ea0d0b829d5a00b3445b580c75a8df0c
katerji.abedalaziz@gmail.com <katerji.abedalaziz@gmail.com>
2023-09-30 12:24:23 +0400

    Fix: select active accounts only

--- db/query/account_queries.go
+++ db/query/account_queries.go
@@ -2,5 +2,5 @@ package query
 
 const InsertAccountQuery = "INSERT INTO account (name, customer_id) VALUES (?, ?)"
 const DepositQuery = "UPDATE account SET balance = balance + ? WHERE id = ?"
-const FetchAccountQuery = "SELECT id, name, balance, customer_id FROM account WHERE id = ?"
+const FetchAccountQuery = "SELECT id, name, balance, customer_id FROM account WHERE id = ? AND deleted = 0"
 const WithdrawQuery = "UPDATE account SET balance = balance - ? WHERE id = ?"
